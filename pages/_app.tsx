85d991f040466821fbf29e706a87e5aa5b962b39
Jacob <Jacob>
2023-06-20 21:44:53 -0600

    wrapped in playerProvider component

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import PlayerProvider from "@/context/PlayerContext";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <PlayerProvider>
+      <Component {...pageProps} />
+    </PlayerProvider>
+  );
+}
