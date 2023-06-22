ad80ec72697870d2ea9a41841133c51ec400dfc0
Jacob <Jacob>
2023-06-22 11:02:55 -0600

    added start page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useReducer, useState } from "react";
 
+import Header from "@/components/layout/Header";
 import Layout from "@/components/layout/Layout";
 import Page from "@/components/layout/Page";
 import Tile from "@/components/Tile";
@@ -21,6 +22,9 @@ const Home = ({ tracks }: HomeProps) => {
     moves: 10,
     solved: [],
   });
+
+  const [gameStart, setGameStart] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
 
   const { setCurrentTrack } = usePlayer();
 
@@ -62,32 +66,62 @@ const Home = ({ tracks }: HomeProps) => {
         startPct: state.tiles[state.playingTile].startPct,
         endPct: state.tiles[state.playingTile].endPct,
       });
+    } else {
+      setCurrentTrack(null);
     }
   }, [state.playingTile, state.tiles, setCurrentTrack]);
 
   return (
     <Layout>
-      <Page title="Home">
-        <div className="flex justify-center w-screen pt-8 sm:pt-0">
-          <div className="grid grid-cols-4 gap-4">
-            {state.tiles.map((tile, index) => (
-              <Tile
-                key={index}
-                id={index}
-                showTile={!tile.isSolved}
-                isPlaying={tile.isPlaying}
-                isSelected={state.selectedTiles.includes(index)}
-                onClick={handleTileClick}
-              />
-            ))}
+      {gameStart ? (
+        <>
+          <Header />
+          <Page title="Home">
+            <div className="flex justify-center w-screen pt-8 sm:pt-0">
+              <div className="grid grid-cols-4 gap-2 sm:gap-4">
+                {state.tiles.map((tile, index) => (
+                  <Tile
+                    key={index}
+                    id={index}
+                    showTile={!tile.isSolved}
+                    isPlaying={tile.isPlaying}
+                    isSelected={state.selectedTiles.includes(index)}
+                    onClick={handleTileClick}
+                  />
+                ))}
+              </div>
+            </div>
+            <div
+              className={`py-8 w-full text-center text-4xl font-light ${scoreColor}`}
+            >
+              {state.moves}
+            </div>
+          </Page>
+        </>
+      ) : (
+        <Page title="Start">
+          <div className="flex flex-col h-screen bg-aliceblue px-2 justify-center">
+            <h1 className="text-4xl pb-4 text-center font-light">musicmatch</h1>
+            <h3 className="text-2xl text-center font-light pb-4">
+              Can you match all eight songs?
+            </h3>
+            <div className="flex flex-col sm:flex-row justify-center">
+              <button
+                className="m-4 text-white bg-orange-400 hover:bg-orange-600 py-2 px-8 rounded"
+                onClick={() => setGameStart(true)}
+              >
+                Start Game
+              </button>
+              <button className="m-4 py-2 px-8 rounded border-2 border-slate-600">
+                How to Play
+              </button>
+              <button className="m-4 py-2 px-8 rounded border-2 border-slate-600">
+                Submit Puzzle
+              </button>
+            </div>
           </div>
-        </div>
-        <div
-          className={`py-8 w-full text-center text-4xl font-light ${scoreColor}`}
-        >
-          {state.moves}
-        </div>
-      </Page>
+        </Page>
+      )}
     </Layout>
   );
 };
