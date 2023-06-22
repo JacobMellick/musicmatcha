76a4c999a425b5e27cc054e63c7fc2617d3706ff
Jacob <Jacob>
2023-06-22 11:03:25 -0600

    removed header from default layout, also removed unecessary classes

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,13 +1,8 @@
-import Header from "@/components/layout/Header";
-
 const Layout: React.ComponentType<React.PropsWithChildren<unknown>> = ({
   children,
 }) => (
   <div className="flex min-h-screen flex-col bg-white">
-    <Header />
-    <div className="z-0 flex grow flex-col py-6">
-      <div className="z-0 flex grow flex-col">{children}</div>
-    </div>
+    <div className="z-0 flex grow flex-col">{children}</div>
   </div>
 );
 
