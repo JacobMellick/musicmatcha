3b321a2bca690c4f3bc1881763789a2e9de93c58
Jacob <Jacob>
2023-06-22 19:03:13 -0600

    made score rendering automatic (only works with even scores)

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,8 @@ import { usePlayer } from "@/context/PlayerContext";
 
 import { reducer } from "@/components/reducers/game";
 
+import { NUM_MOVES } from "@/lib/constants";
+
 type HomeProps = {
   tracks: Track[];
 };
@@ -19,37 +21,34 @@ const Home = ({ tracks }: HomeProps) => {
     tracks,
     tiles: [],
     selectedTiles: [],
-    moves: 10,
+    moves: NUM_MOVES,
     solved: [],
   });
 
-  const [gameStart, setGameStart] = useState(false);
   const [gameOver, setGameOver] = useState(false);
 
   const { setCurrentTrack } = usePlayer();
 
-  const colorList = [
-    "text-[#77A769]",
-    "text-[#84955E]",
-    "text-[#8E8756]",
-    "text-[#97794D]",
-    "text-[#A26944]",
-    "text-[#AE5739]",
-    "text-[#BB452E]",
-    "text-[#C63424]",
-    "text-[#D1231A]",
-    "text-[#E00D0D]",
-  ];
-
-  const [scoreColor, setScoreColor] = useState(colorList[0]);
-
-  const handleScoreChange = () => {
-    setScoreColor(() => {
-      return colorList[colorList.length - state.moves];
-    });
+  const [scoreColor, setScoreColor] = useState("from-100%");
+  const handleScoreColor = () => {
+    let direction = "";
+    if (state.moves > Math.floor(NUM_MOVES / 2)) {
+      direction = "from";
+    } else {
+      direction = "to";
+    }
+    let value =
+      ((state.moves % Math.floor(NUM_MOVES / 2)) / Math.floor(NUM_MOVES / 2)) *
+      100;
+    if (value == 0) {
+      value = 100;
+    }
+    let colorString = direction + "-" + value + "%";
+    return setScoreColor(() => colorString);
   };
-
-  useEffect(handleScoreChange, [state.moves]);
+  useEffect(() => {
+    handleScoreColor();
+  }, [state.moves]);
 
   const handleTileClick = (id: number) => {
     dispatch({ type: "tileClick", payload: id });
@@ -73,55 +72,33 @@ const Home = ({ tracks }: HomeProps) => {
 
   return (
     <Layout>
-      {gameStart ? (
-        <>
-          <Header />
-          <Page title="Home">
-            <div className="flex justify-center w-screen pt-8 sm:pt-0">
-              <div className="grid grid-cols-4 gap-2 sm:gap-4">
-                {state.tiles.map((tile, index) => (
-                  <Tile
-                    key={index}
-                    id={index}
-                    showTile={!tile.isSolved}
-                    isPlaying={tile.isPlaying}
-                    isSelected={state.selectedTiles.includes(index)}
-                    onClick={handleTileClick}
-                  />
-                ))}
-              </div>
-            </div>
-            <div
-              className={`py-8 w-full text-center text-4xl font-light ${scoreColor}`}
-            >
-              {state.moves}
-            </div>
-          </Page>
-        </>
-      ) : (
-        <Page title="Start">
-          <div className="flex flex-col h-screen bg-aliceblue px-2 justify-center">
-            <h1 className="text-4xl pb-4 text-center font-light">musicmatch</h1>
-            <h3 className="text-2xl text-center font-light pb-4">
-              Can you match all eight songs?
-            </h3>
-            <div className="flex flex-col sm:flex-row justify-center">
-              <button
-                className="m-4 text-white bg-orange-400 hover:bg-orange-600 py-2 px-8 rounded"
-                onClick={() => setGameStart(true)}
-              >
-                Start Game
-              </button>
-              <button className="m-4 py-2 px-8 rounded border-2 border-slate-600">
-                How to Play
-              </button>
-              <button className="m-4 py-2 px-8 rounded border-2 border-slate-600">
-                Submit Puzzle
-              </button>
-            </div>
+      <Header />
+      <Page title="Home">
+        <div className="flex justify-center w-screen pt-8 sm:pt-0">
+          <div className="grid grid-cols-4 gap-2 sm:gap-4">
+            {state.tiles.map((tile, index) => (
+              <Tile
+                key={index}
+                id={index}
+                showTile={!tile.isSolved}
+                isPlaying={tile.isPlaying}
+                isSelected={state.selectedTiles.includes(index)}
+                onClick={handleTileClick}
+              />
+            ))}
           </div>
-        </Page>
-      )}
+        </div>
+        <div
+          className={`${
+            state.moves > 0
+              ? `content-center pt-8 text-center text-4xl bg-gradient-to-r bg-clip-text text-transparent from-green-500 ${scoreColor} to-red-500`
+              : "invisible"
+          }`}
+        >
+          {state.moves}
+        </div>
+        <div className="py-4 flex justify-center"></div>
+      </Page>
     </Layout>
   );
 };
