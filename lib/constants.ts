c79c33a95d8408555480d4d806352a8ef0498764
Jacob <Jacob>
2023-06-22 19:03:33 -0600

    changed game name

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
-export const DEFAULT_TITLE = "musicmatch";
+export const DEFAULT_TITLE = "musicmatcha";
 
 export const NUM_SQUARES = 16;
 export const NUM_SONGS = 8;
 export const GRID_SIZE = 4;
 
-export const NUM_MOVES = 10;
+export const NUM_MOVES = 20;
 
 export const TRACK_LENGTH = 4;
