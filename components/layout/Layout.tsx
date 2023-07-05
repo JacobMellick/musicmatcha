2807562c69adbc6308e101b1659c6deed96a5ae2
Jacob <Jacob>
2023-07-05 14:16:09 -0600

    removed unecessary tailwind classes

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,7 +2,7 @@ const Layout: React.ComponentType<React.PropsWithChildren<unknown>> = ({
   children,
 }) => (
   <div className="flex min-h-screen flex-col bg-white">
-    <div className="z-0 flex grow flex-col">{children}</div>
+    <div className="z-0 flex flex-col">{children}</div>
   </div>
 );
 
