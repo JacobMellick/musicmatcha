da7c2832fd18810875784a95bd3041f47fe04ec7
Jacob <Jacob>
2023-07-05 11:55:05 -0600

    built Card component and component functionality

--- components/cards/Card.tsx
+++ components/cards/Card.tsx
@@ -1,38 +1,66 @@
-import StopIcon from "@/components/icons/StopIcon";
 import { PlayIcon } from "@heroicons/react/24/outline";
-import PauseIcon from "@/components/icons/PauseIcon";
-import { use, useState } from "react";
+import { PauseIcon } from "@heroicons/react/24/outline";
 
 type CardProps = {
+  id: number;
   name: string;
   track_url: string;
+  preview_url: string;
   artists: {
     name: string;
     artist_url: string;
   }[];
-  onClick: any;
+  onClick: (id: string) => void;
+  isPlaying?: boolean;
+  found: boolean;
 };
 
-const Card = ({ name, track_url, artists, onClick }: CardProps) => {
-  const [isPlaying, setIsPlaying] = useState(true);
+const Card = ({
+  id,
+  name,
+  track_url,
+  preview_url,
+  artists,
+  onClick,
+  isPlaying = false,
+  found,
+}: CardProps) => {
   return (
-    <div className="flex border-2 rounded-md mx-8 p-2 sm:max-w-sm justify-between align-center">
-      <div className="">
-        <a href={track_url} className="text-sm">
-          {name}
-        </a>
-        <div>
+    <div
+      className={`flex rounded-2xl p-2 w-full justify-between ${
+        found ? "bg-green-200" : "bg-red-200"
+      } items-center`}
+    >
+      <div className="pl-2 whitespace-nowrap">
+        <p className="w-[30ch] text-ellipsis overflow-hidden text-sm">
+          <a href={track_url} target="_blank">
+            {name}
+          </a>
+        </p>
+
+        <p className="w-[30ch] text-ellipsis overflow-hidden">
           {artists.map((artist) => (
-            <a className="text-gray-500 text-xs" href={artist.artist_url}>
+            <a
+              className="text-gray-500 text-xs hover:underline"
+              href={artist.artist_url}
+              target="_blank"
+            >
               {artist.name}{" "}
             </a>
           ))}
-        </div>
+        </p>
       </div>
 
-      <button className="border-2 rounded-full w-8 h-8" onClick={onClick}>
-        <PlayIcon className="text-gray-600"></PlayIcon>
-      </button>
+      <div
+        className="flex hover:cursor-pointer"
+        onClick={() => onClick(preview_url)}
+      >
+        {isPlaying ? (
+          <PauseIcon className="text-gray-600 w-8 h-8"></PauseIcon>
+        ) : (
+          <PlayIcon className="text-gray-600 w-8 h-8"></PlayIcon>
+        )}
+      </div>
     </div>
   );
 };
