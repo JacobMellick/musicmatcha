c12d8b1aa8d37412ca75da91d70494a95d0e4feb
Jacob <Jacob>
2023-07-31 19:29:39 -0600

    removed framermotion, gearModal, implemented localStorage

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,25 +1,70 @@
 import {
   InformationCircleIcon,
   ChartBarIcon,
-  Cog6ToothIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 
 import { DEFAULT_TITLE } from "@/lib/constants";
-import { NUM_MOVES } from "@/lib/constants";
 
-import { motion } from "framer-motion";
-
-import { useState, Fragment } from "react";
+import { useState, Fragment, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import Card from "../cards/Card";
+import { Track } from "@/types/proj01";
 
 const Header = () => {
-  let [infoModal, setInfoModal] = useState(true);
-  let [statsModal, setStatsModal] = useState(false);
-  let [gearModal, setGearModal] = useState(false);
+  let [openAbout, setOpenAbout] = useState(true);
+
+  useEffect(() => {
+    const localRecorded = localStorage.getItem("recorded");
+    if (localRecorded === null) {
+      localStorage.setItem("recorded", JSON.stringify("no"));
+    } else if (JSON.parse(localRecorded) === "yes") {
+      console.log("openAboutisChanging!!");
+      setOpenAbout(false);
+      console.log(openAbout);
+    }
+  }, []);
+
+  let [infoModal, setInfoModal] = useState(openAbout);
+  let [statsModal, setStatsModal] = useState(!openAbout);
+  let [solved, setSolved] = useState<Track[]>([]);
+  let [showSolved, setShowSolved] = useState(false);
+  let [played, setPlayed] = useState(0);
+  let [winrate, setWinrate] = useState("0%");
+  let [streak, setStreak] = useState(0);
+
+  useEffect(() => {
+    const localPlays = localStorage.getItem("plays");
+    const localWins = localStorage.getItem("wins");
+    const localStreak = localStorage.getItem("streak");
+
+    if (localPlays !== null && localWins !== null && localStreak !== null) {
+      let wins = JSON.parse(localWins);
+      let plays = JSON.parse(localPlays);
+      let numWinrate = Math.ceil((wins / plays) * 100);
+      setWinrate(numWinrate + "%");
+      setPlayed(plays);
+      setStreak(JSON.parse(localStreak));
+    }
+  }, [statsModal]);
+
+  useEffect(() => {
+    if (solved.length !== 0) {
+      setShowSolved(true);
+    }
+  }, [statsModal]);
+
+  useEffect(() => {
+    const localSolved = localStorage.getItem("solved");
+    if (localSolved === null) {
+      setSolved([]);
+    } else {
+      setSolved(JSON.parse(localSolved));
+    }
+  }, [statsModal]);
 
   return (
-    <div className="flex w-full flex-start p-4 shadow-sm justify-between mb-8">
+    <div className="flex w-full flex-start p-4 shadow-sm justify-between mb-4">
       <div className="text-green-500 font-bold">{DEFAULT_TITLE}</div>
       <div className="flex space-x-2">
         <InformationCircleIcon
@@ -36,14 +81,6 @@ const Header = () => {
           className="text-gray-600 hover:text-gray-800 hover:cursor-pointer"
           onClick={() => {
             setStatsModal(true);
-          }}
-        />
-        <Cog6ToothIcon
-          height={24}
-          width={24}
-          className="text-gray-600 hover:text-gray-800 hover:cursor-pointer"
-          onClick={() => {
-            setGearModal(true);
           }}
         />
       </div>
@@ -86,7 +123,7 @@ const Header = () => {
                   </Dialog.Title>
                   <div className="mt-2 mb-4">
                     <p className="text-sm text-gray-500">
-                      Can you match all eight songs in twenty tries?
+                      Can you match all eight songs in ten tries?
                     </p>
                   </div>
                   <h3 className="text-md">How to Play</h3>
@@ -142,93 +179,53 @@ const Header = () => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <XMarkIcon
+                    height={18}
+                    width={18}
+                    className="absolute right-4 top-4 text-gray-500 hover:text-gray-600 hover:cursor-pointer"
+                    onClick={() => {
+                      setStatsModal(false);
+                    }}
+                  />
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
                     Stats
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500"></p>
+                  <div className="flex mt-2 justify-between px-24 mb-2 text-center">
+                    <div>
+                      <div className="text-xl">{played}</div>
+                      <div className="text-xs">Played</div>
+                    </div>
+                    <div>
+                      <div className="text-xl">{winrate}</div>
+                      <div className="text-xs">Winrate</div>
+                    </div>
+                    <div>
+                      <div className="text-xl">{streak}</div>
+                      <div className="text-xs">Streak</div>
+                    </div>
                   </div>
-                  <h3 className="text-md">Songs Guessed</h3>
-
-                  <p className="text-sm text-gray-500 mt-1">
-                    Click on a square to play a short clip from a song.
-                    Selecting two clips from the same song will clear the
-                    matching squares from the grid.
-                  </p>
-                  <h3 className="text-md">Songs Missed</h3>
-
-                  <p className="text-sm text-gray-500 mt-1">
-                    Click on a square to play a short clip from a song.
-                    Selecting two clips from the same song will clear the
-                    matching squares from the grid.
-                  </p>
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                    >
-                      Go to Playlist
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-
-      <Transition appear show={gearModal} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={() => setGearModal(false)}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Settings
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">1234</p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                      onClick={() => setGearModal(false)}
-                    >
-                      Resume
-                    </button>
-                  </div>
+                  {showSolved && (
+                    <>
+                      <h3 className="text-md">Today's Songs Found</h3>
+                      <div className="pt-2 flex flex-col gap-2">
+                        {solved.map((card) => (
+                          <Card
+                            id={card.id}
+                            name={card.name}
+                            track_url={card.track_url}
+                            preview_url={card.preview_url}
+                            found={true}
+                            artists={card.artists}
+                            onClick={() => {}}
+                            playable={false}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  )}
                 </Dialog.Panel>
               </Transition.Child>
             </div>
