a03ebac2648fd8cdd30cbf7ee6fdada27916348d
Jacob <Jacob>
2023-09-01 16:39:32 -0600

    moved puzzles, slightly adjusted chart

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,32 +1,35 @@
-type ChartProps =  {stats: Record<number, number>};
+type ChartProps = { stats: Record<number, number> };
 
 const Chart = (stats: ChartProps) => {
-  const max = Math.max(...Object.values(stats.stats).map(Number)) === 0 ? 1 : Math.max(...Object.values(stats.stats).map(Number))
-
-    return <div className="px-12 pt-4 text-xs text-right">
-        {Object.keys(stats.stats).map(key => {
-
-          const numKey = parseInt(key)
-
-          const val = stats.stats[numKey]
-
-          const width = val !== 0 ? Math.round(val / max * 100): 2
-
-          const title = val.toString()
-
-
+  const max =
+    Math.max(...Object.values(stats.stats).map(Number)) === 0
+      ? 1
+      : Math.max(...Object.values(stats.stats).map(Number));
 
   return (
-    <div
-      key={key}
-      className={`flex py-1 justify-content items-center`}
-    >
-      <span className="pr-4 w-6">{key}</span>
-      <div style={{ width: `${width}%` }} className={`bg-gray-300 rounded-r-lg h-2`} title={title}></div>
+    <div className="px-12 pt-4 text-xs text-right">
+      {Object.keys(stats.stats).map((key) => {
+        const numKey = parseInt(key);
+
+        const val = stats.stats[numKey];
+
+        const width = val !== 0 ? Math.round((val / max) * 100) : 2;
+
+        const title = val.toString();
+
+        return (
+          <div key={key} className={`flex py-1 justify-content items-center`}>
+            <span className="pr-4 w-6">{key}</span>
+            <div
+              style={{ width: `${width}%` }}
+              className={`bg-gray-300 rounded-r-sm h-4`}
+              title={title}
+            ></div>
+          </div>
+        );
+      })}
     </div>
   );
-})}
-    </div>
-}
+};
 
 export default Chart;
